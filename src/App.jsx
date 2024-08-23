5d3b70a4c3ce039469ef259895db0fabfc4b2c85
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-08-23 13:54:02 +0300

    v.3.1.11

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ function App() {
   // константа contactArr отримує масив contactUsers та далі передаємо в компонент ContactList
   const [contactArr, setContactAdd] = useState(()=>{
     const contactArrStorage = window.localStorage.getItem("save-contact");
-    if (contactArrStorage.length > 2) {
-      console.log("contactArrStorage.length", contactArrStorage.length);
-      return JSON.parse(contactArrStorage);
+    const contactUsersParse = JSON.parse(contactArrStorage);
+    if (contactUsersParse.length > 0) {
+      // console.log("contactArrStorage.length", contactArrStorage.length);
+      return contactUsersParse;
     }
-    console.log("contactArrStorage.length", contactArrStorage);
-    return contactUsersStart;
+    // console.log("contactArrStorage.length", contactArrStorage);
+     return contactUsersStart ;
    });
   
   
